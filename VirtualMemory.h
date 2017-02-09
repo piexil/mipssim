21db2fc1b3648c175ebdcd84d1c6bf4db35079b2
Robert Ross <fragger105@gmail.com>
2017-02-09 18:38:19 -0500

    vm stuff

--- VirtualMemory.h
+++ VirtualMemory.h
@@ -2,7 +2,7 @@
 #define VIRTUALMEMORY_H
 #include "Common.h"
 
-int memsize = 67108864; //default to 64MiB ram
+int memsize = 67108864/sizeof(_DATA_WORD); //default to 64MiB ram
 typedef struct _VirtualMemory{
 	union{
 		_DATA_WORD* mem; //get mem via word size
@@ -12,6 +12,7 @@ typedef struct _VirtualMemory{
 
 
 VirtualMem* mem_Create();
+void mem_destroy(VirtualMem* vm);
 void vmem_set_word(VirtualMem* vm, int position, _DATA_WORD val);
 _DATA_WORD vmem_get_word(VirtualMem* vm, int position);
 #endif
